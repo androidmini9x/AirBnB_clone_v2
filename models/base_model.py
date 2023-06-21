104ceac086a9ea2c02e7fb2f17c871bd9163799b
Androidmini9x <androidmini92@gmail.com>
2023-06-22 00:00:33 +0300

    Fixed

--- models/base_model.py
+++ models/base_model.py
@@ -77,7 +77,7 @@ class BaseModel:
         my_dict["created_at"] = self.created_at.isoformat()
         my_dict["updated_at"] = self.updated_at.isoformat()
 
-        if '_sa_instance_state' in my_dict:
+        if my_dict['_sa_instance_state']:
             my_dict.pop('_sa_instance_state')
 
         return my_dict
