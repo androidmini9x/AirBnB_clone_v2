f88d8ceac520097d867cd72a33cee800ee9001d0
Androidmini9x <androidmini92@gmail.com>
2023-07-06 17:08:34 +0300

    Fixed

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-from datetime import datetime
+"""Fabric script that generates a .tgz archive"""
+
 from fabric.api import local
+import time
 
 
 def do_pack():
-    """Generates a .tgz archive from the contents
-    of the web_static folder of this repository.
-    """
-
-    d = datetime.now()
-    now = d.strftime('%Y%m%d%H%M%S')
-
-    local("mkdir -p versions")
-    local("tar -czvf versions/web_static_{}.tgz web_static".format(now))
+    time_string = time.strftime("%Y%m%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{}.tgz web_static/".
+              format(time_string))
+        return ("versions/web_static_{}.tgz".format(time_string))
+    except:
+        return None
