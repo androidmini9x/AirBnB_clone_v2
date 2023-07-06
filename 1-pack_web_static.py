16f7ed9faecd9e1a4e40d3a0f495b6c10187e53f
atck3r <atck3r@localdomain>
2023-07-07 00:05:08 +0300

    Fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,28 +1,21 @@
 #!/usr/bin/python3
-"""Generate .tgz file from the contents of the web_static folder"""
+'''Fabric script to generate .tgz archive'''
 
-from fabric import api
+from fabric.api import local
 from datetime import datetime
-import os
+
+from fabric.decorators import runs_once
 
 
+@runs_once
 def do_pack():
-    """Function to create tarball of webstatic files from the web_static
-    folder in Airbnb_v2.
+    '''generates .tgz archive from the contents of the web_static folder'''
+    local("mkdir -p versions")
+    path = ("versions/web_static_{}.tgz"
+            .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
+    result = local("tar -cvzf {} web_static"
+                   .format(path))
 
-    Returns: path of .tgz file on success, None otherwise
-    """
-    with api.settings(warn_only=True):
-        isdir = os.path.isdir('versions')
-        if not isdir:
-            mkdir = api.local('mkdir versions')
-            if mkdir.failed:
-                return None
-        suffix = datetime.now().strftime('%Y%m%d%H%M%S')
-        path = 'versions/web_static_{}.tgz'.format(suffix)
-        tar = api.local('tar -cvzf {} web_static'.format(path))
-        if tar.failed:
-            return None
-        size = os.stat(path).st_size
-        print('web_static packed: {} -> {}Bytes'.format(path, size))
-        return path
+    if result.failed:
+        return None
+    return path
