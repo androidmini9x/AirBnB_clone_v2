d4786eb5852ece09fb09ad25ca13191934e6b147
atck3r <atck3r@localdomain>
2023-07-07 22:11:53 +0300

    F

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-"""A module for web application deployment with Fabric."""
-import os
-from time import strftime
-from fabric.api import local, runs_once
+'''Fabric script to generate .tgz archive'''
+
+from fabric.api import local
+from datetime import datetime
+
+from fabric.decorators import runs_once
 
 
 @runs_once
 def do_pack():
-    """Archives the static files."""
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
+    '''generates .tgz archive from the contents of the web_static folder'''
+    local("mkdir -p versions")
+    path = ("versions/web_static_{}.tgz"
+            .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
+    result = local("tar -cvzf {} web_static"
+                   .format(path))
 
-    filename = strftime("%Y%m%d%H%M%S")
-    output = "versions/web_static_{}.tgz".format(filename)
-    try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        archize_size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
-    except Exception:
-        output = None
-    return output
+    if result.failed:
+        return None
+    return path
