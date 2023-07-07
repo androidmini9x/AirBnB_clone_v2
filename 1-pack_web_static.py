18ad3fbea027b215a5a267eb08f85d48d32f5a2d
atck3r <atck3r@localdomain>
2023-07-07 22:41:06 +0300

    ffX

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,10 +10,13 @@ from datetime import datetime
 def do_pack():
     """ Fabric script that generates a .tgz archive from the contents of the...
     ...web_static folder """
-    local("sudo mkdir -p versions")
+    if local("sudo mkdir -p versions").failed:
+        return None
+
     date = datetime.now().strftime("%Y%m%d%H%M%S")
     filename = "versions/web_static_{}.tgz".format(date)
     result = local("sudo tar -cvzf {} web_static".format(filename))
+
     if result.succeeded:
         return filename
     else:
